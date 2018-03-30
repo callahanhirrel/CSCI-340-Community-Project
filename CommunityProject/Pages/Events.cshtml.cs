2143184a99e2b834ce961b44c6546171dfbc1779
callahanhirrel <callahan.hirrel@gmail.com>
2018-03-30 15:13:57 -0500

    hopefully its all fixed up

--- CommunityProject/Pages/Events.cshtml.cs
+++ CommunityProject/Pages/Events.cshtml.cs
@@ -12,7 +12,7 @@ namespace CommunityProject.Pages
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "";
         }
     }
 }
