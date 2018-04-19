94c5e1744e1bfbd1c24718f19a122686fed5b880
callahanhirrel <callahan.hirrel@gmail.com>
2018-04-19 13:20:17 -0500

    working on connecting DB to pages

--- CommunityProject/Models/GenInfo.cs
+++ CommunityProject/Models/GenInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace CommunityProject.Models
 {
     public class GenInfo
@@ -6,6 +8,9 @@ namespace CommunityProject.Models
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
         public string Email { get; set; }
         public int Phone { get; set; }
         public string Address { get; set; }
---
+++ CommunityProject/Models/SeedData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CommunityProject.Models
+{
+    public static class SeedData
+    {
+        
+    }
+}
