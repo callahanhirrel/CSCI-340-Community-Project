ae13aadcb769b6bf64af6dc615f89c8dbfdd45bf
CDanyluk <chantaldanyluk@yahoo.com>
2018-04-22 16:35:39 -0500

    Revert "xb"
    This reverts commit 9219af63cf3d57a5f2f8d72c394b8f970af349f7.

--- CommunityProject/Startup.cs
+++ CommunityProject/Startup.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using CommunityProject.Models;
 
 namespace CommunityProject
 {
@@ -23,10 +21,7 @@ namespace CommunityProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
-            services.AddDbContext<DatabaseContext>(options =>
-                  options.UseSqlite(Configuration.GetConnectionString("DatabaseContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
